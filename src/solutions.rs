4b88aa6bfa3fbefbb9c40cada6605753731d57ac
burjee <burjee1105@gmail.com>
2023-05-12 11:21:14 +0800

    add 2140

--- src/solutions.rs
+++ src/solutions.rs
@@ -48,6 +48,7 @@ pub mod p21;
 pub mod p211;
 pub mod p212;
 pub mod p213;
+pub mod p2140;
 pub mod p217;
 pub mod p2215;
 pub mod p226;
@@ -226,6 +227,7 @@ pub fn run(n: &str) {
         "1657" => p1657::run(),
         "1822" => p1822::run(),
         "2215" => p2215::run(),
+        "2140" => p2140::run(),
         "2336" => p2336::run(),
         _ => println!("Solution not found."),
     }
---
+++ src/solutions/p2140.rs
@@ -0,0 +1,50 @@
+struct Solution {}
+impl Solution {
+    pub fn most_points(questions: Vec<Vec<i32>>) -> i64 {
+        let len = questions.len();
+        let mut ans = vec![0; len + 1];
+        for i in (0..len).rev() {
+            let power = questions[i][1] as usize;
+            let point = questions[i][0] as i64 + ans.get(i + power + 1).unwrap_or(&0);
+            ans[i] = ans[i + 1].max(point);
+        }
+        ans[0]
+    }
+
+    pub fn _most_points(questions: Vec<Vec<i32>>) -> i64 {
+        Solution::_helper(&questions, &mut vec![-1; questions.len()], 0)
+    }
+
+    pub fn _helper(questions: &Vec<Vec<i32>>, cache: &mut Vec<i64>, i: usize) -> i64 {
+        if i >= questions.len() {
+            return 0;
+        }
+        if cache[i] != -1 {
+            return cache[i];
+        }
+
+        let point = questions[i][0] as i64;
+        let power = questions[i][1] as usize;
+        let sum1 = point + Solution::_helper(questions, cache, i + power + 1);
+        let sum2 = Solution::_helper(questions, cache, i + 1);
+        cache[i] = sum1.max(sum2);
+        cache[i]
+    }
+}
+
+pub fn run() {
+    let inputs = [
+        vec![vec![3, 2], vec![4, 3], vec![4, 4], vec![2, 5]],
+        vec![vec![1, 1], vec![2, 2], vec![3, 3], vec![4, 4], vec![5, 5]],
+        vec![
+            vec![1, 1],
+            vec![2, 2],
+            vec![3, 3],
+            vec![4, 4],
+            vec![999, 999],
+        ],
+    ];
+    for questions in inputs {
+        println!("{:?}", Solution::most_points(questions));
+    }
+}
