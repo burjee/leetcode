bd31450e608524b6f621fbd7e2a8aac2f86dc31d
burjee <burjee1105@gmail.com>
2024-07-22 11:37:18 +0800

    add 199 bfs

--- src/solutions/p199.rs
+++ src/solutions/p199.rs
@@ -6,20 +6,41 @@ struct Solution {}
 impl Solution {
     pub fn right_side_view(root: Option<Rc<RefCell<TreeNode>>>) -> Vec<i32> {
         let mut ans = vec![];
-        Solution::helper(&root, &mut ans, 0);
+        let mut nodes = std::collections::VecDeque::new();
+        let mut level = 0;
+        nodes.push_back(root);
+        while !nodes.is_empty() {
+            for _ in 0..nodes.len() {
+                if let Some(node) = nodes.pop_front().unwrap() {
+                    let mut node = node.borrow_mut();
+                    if level == ans.len() {
+                        ans.push(node.val);
+                    }
+                    nodes.push_back(node.right.take());
+                    nodes.push_back(node.left.take());
+                }
+            }
+            level += 1;
+        }
         ans
     }
 
-    pub fn helper(root: &Option<Rc<RefCell<TreeNode>>>, ans: &mut Vec<i32>, level: usize) {
-        if let Some(node) = root {
-            let node = node.borrow();
-            if level == ans.len() {
-                ans.push(node.val);
-            }
-            Solution::helper(&node.right, ans, level + 1);
-            Solution::helper(&node.left, ans, level + 1);
-        }
-    }
+    // pub fn right_side_view(root: Option<Rc<RefCell<TreeNode>>>) -> Vec<i32> {
+    //     let mut ans = vec![];
+    //     Solution::helper(&root, &mut ans, 0);
+    //     ans
+    // }
+
+    // pub fn helper(root: &Option<Rc<RefCell<TreeNode>>>, ans: &mut Vec<i32>, level: usize) {
+    //     if let Some(node) = root {
+    //         let node = node.borrow();
+    //         if level == ans.len() {
+    //             ans.push(node.val);
+    //         }
+    //         Solution::helper(&node.right, ans, level + 1);
+    //         Solution::helper(&node.left, ans, level + 1);
+    //     }
+    // }
 }
 
 pub fn run() {
